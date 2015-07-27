d666bbe33aa5db0ed61737a37407757b6e21e6d7
Sam Couch <samcouch91@gmail.com>
2015-07-27 14:23:03 -0400

    Onward to Wimbledon!

--- main.go
+++ main.go
@@ -4,16 +4,26 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 var port int
+
+var names = []string{
+	"Serena Williams",
+	"Roger Federer",
+	"Andy Murray",
+	"Maria Sharapova",
+	"Rafael Nadal",
+}
 
 func main() {
 	flag.IntVar(&port, "p", 3000, "The port to run serve up your files on.")
 	flag.Parse()
 
-	fmt.Printf("Serving aces on port %v\n", port)
+	fmt.Printf("Move aside %v! We're serving up aces on port %v\n", RandName(names), port)
 
 	err := AceServe(port)
 
@@ -26,4 +36,9 @@ func AceServe(port int) error {
 	host := fmt.Sprintf("localhost:%v", port)
 
 	return http.ListenAndServe(host, http.FileServer(http.Dir(".")))
+}
+
+func RandName(names []string) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return names[rand.Intn(len(names))]
 }
